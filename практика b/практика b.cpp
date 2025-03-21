12fc5ad1d7ac2eeec8e95f92d6129e727c742a17
Tatiana164 <hikitinskaia@yandex.ru>
2025-03-21 12:32:51 +0300

    ф

---
+++ практика b/практика b.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath> // Для использования математических функций: sin, cos, exp, log
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    float x, y;
+
+    // Ввод значений x и y
+    std::cout << "Введите значение x: ";
+    std::cin >> x;
+    std::cout << "Введите значение y: ";
+    std::cin >> y;
+
+    // Расчет R по формуле: R = 2 * sin((x + y) / 2) * cos((x - y) / 2)
+    float R = 2 * sin((x + y) / 2) * cos((x - y) / 2);
+
+    // Расчет S по формуле: S = e^2 * ln(x)
+    float S = exp(2) * log(x);
+
+    // Вывод значений R и S
+    std::cout << "R = " << R << std::endl;
+    std::cout << "S = " << S << std::endl;
+
+    // Нахождение максимального значения между R и S
+    float C = std::max(R, S);
+
+    // Вывод максимального значения
+    std::cout << "Максимальное значение (C) = " << C << std::endl;
+
+    return 0;
+}
